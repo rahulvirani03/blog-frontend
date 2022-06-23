abcc28fccee5061dc7e285f7c4f1271396606540
rahulvirani03 <rahulsvirani25@gmail.com>
2022-06-23 12:08:27 +0530

    Blog Prototype Create

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,8 @@
 import axios from "axios";
+//const localURL = "http://localhost:4000";
+const deployedURL = "https://blogg-backend.herokuapp.com";
+const baseURL = `${deployedURL}/api`;
 
-const baseURL = "http://localhost:4000/api";
 const token = localStorage.getItem("token");
 export const api = axios.create({
   baseURL: baseURL,
